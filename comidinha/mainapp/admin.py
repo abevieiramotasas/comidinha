4df4c3c40475a18ef726f00b58ce2c29f0af2985
abelardo <abbbekgb@gmail.com>
2012-07-02 20:57:53 -0400

    terminado modelos basico, faltando correcao no TODO.txt

--- comidinha/mainapp/admin.py
+++ comidinha/mainapp/admin.py
@@ -1,15 +1,29 @@
 from django.contrib.gis import admin
-from models import DonorHouse, Donor
+from models import DonorHouse, Donor, Distributor, DistributionCenter, Food
 
 # admin.site.register(DonorHouse, admin.GeoModelAdmin)
 
-class ChoiceInline(admin.TabularInline):
+class DonorInline(admin.TabularInline):
     model = Donor
-    extra = 2
-    
+    extra = 1    
 class DonorHouseAdmin(admin.OSMGeoAdmin):
-    inlines = [ChoiceInline]
+    inlines = [DonorInline]
     
-        
+    
+class DonorHouseInline(admin.TabularInline):
+    model = DonorHouse
+    extra = 1
+class DistributorInline(admin.TabularInline):
+    model = Distributor
+    extra = 1
+class DistributionCenterInline(admin.TabularInline):    
+    model = DistributionCenter
+    extra =  1
+class FoodAdmin(admin.OSMGeoAdmin):
+    inlines = [DonorHouseInline, DistributionCenterInline, DistributorInline]    
+    
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Distributor, admin.OSMGeoAdmin)
+admin.site.register(DistributionCenter, admin.OSMGeoAdmin)        
 admin.site.register(DonorHouse, DonorHouseAdmin)
 admin.site.register(Donor, admin.OSMGeoAdmin)
--- comidinha/mainapp/models.py
+++ comidinha/mainapp/models.py
@@ -5,13 +5,17 @@ class DonorHouse(models.Model):
     address = models.PointField()
     # TODO como descrever telefone?
     # > mesmo em models.Donor.phone
+    # > mesmo em models.Distributor.phone
+    # > mesmo em models.DistributionCenter.phone
     phone = models.CharField(max_length=12)
-    login = models.CharField(max_length=12)
+    login = models.CharField(max_length=12, unique=True)
     # TODO como tratar password?
     password = models.CharField(max_length=12)
     public = models.BooleanField()
     # TODO ver como funciona
-    homepage = models.URLField()
+    # TODO unique para um field funciona por tabela, o que nao eh a intencao aqui
+    #      ou retira o homepage como unico, ou o coloca numa tabela, ou faz a verificacao correta
+    homepage = models.URLField(unique=True)
     # TODO limitar tamanho?
     description = models.TextField()
     
@@ -28,3 +32,52 @@ class Donor(models.Model):
     
     def __unicode__(self):
         return self.name
+        
+class Distributor(models.Model):
+    description = models.TextField()
+    homepage = models.URLField(unique=True)
+    phone = models.CharField(max_length=12)
+    # TODO melhor usar PointField ou (lat, lon) ?
+    position = models.PointField()
+    # TODO como descrever capacidade?
+    height = models.IntegerField()
+    volume = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.homepage
+        
+        
+class DistributionCenter(models.Model):
+    description = models.TextField()
+    homepage = models.URLField(unique=True)
+    phone = models.CharField(max_length=12)
+    # TODO como descrever a necessidade de alimentos?!
+    necessity = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.description
+        
+        
+class Food(models.Model):
+    description = models.TextField()
+    donor_house = models.ForeignKey(DonorHouse)
+    # TODO como utilizar unidades de medida diversas?
+    # > mesmo para volume
+    height = models.IntegerField()
+    volume = models.IntegerField()
+    distributor = models.ForeignKey(Distributor, blank=True)
+    distribution_center = models.ForeignKey(DistributionCenter, blank=True)
+    STATUS_OF_DELIVERY = (
+        ('0', 'In donor'), 
+        ('1', 'Distributor allocated'),
+        ('2', 'With distributor'),
+        ('3', 'In distribution center'),
+        ('4', 'Delivered'),    
+    )
+    status = models.CharField(max_length=1, choices=STATUS_OF_DELIVERY)
+    # TODO este campo deve representar periodos nos quais o distributor pode coletar o alimento    
+    availability = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.description
+    
